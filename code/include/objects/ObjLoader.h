b0cf075d0f6a41ccda8c34b6e143bc400429b702
Pau <pau.amigo@enti.cat>
2023-03-13 17:52:40 +0100

    obj load

--- code/include/objects/ObjLoader.h
+++ code/include/objects/ObjLoader.h
@@ -1,12 +1,14 @@
 #pragma once
 #include <vector>
+#include <string>
 #include <glm/detail/type_vec.hpp>
+#include <glm/glm.hpp>
 
 class ObjLoader
 {
 public:
-	bool  loadOBJ(
-		const char*,
+	bool loadOBJ(
+		const char* path,
 		std::vector<glm::vec3>& out_vertices,
 		std::vector<glm::vec3>& out_uvs,
 		std::vector<glm::vec3>& out_normals
